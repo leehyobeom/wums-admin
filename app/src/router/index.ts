1ba3cbf914cb733beb596a01042a9afb5a668369
HyoBeom <hyobeom@codemind.co.kr>
2022-11-14 20:36:14 +0900

    feat: add settings

---
+++ app/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/views/DashBoard.vue";
+import Monitor from "@/views/Monitor.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/monitor",
+    name: "Monitor",
+    component: Monitor,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
