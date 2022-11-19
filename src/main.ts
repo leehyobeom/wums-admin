706480939c8e67655bc2dce3c36e95e8a0f53ffe
HyoBeom <hyobeom@codemind.co.kr>
2022-11-20 03:37:39 +0900

    feat: add apollo

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,21 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+const cache = new InMemoryCache()
 
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'http://localhost:3000/graphql',
+})
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
 loadFonts()
 
 createApp(App)
+  .use(apolloProvider)
   .use(router)
   .use(vuetify)
   .mount('#app')
