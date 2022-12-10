37ec949d46668ec6b21d1d29ad89bf09235d0e61
HyoBeom <hyobeom@codemind.co.kr>
2022-12-10 11:54:49 +0900

    feat: gql endpoint change

--- apollo.config.js
+++ apollo.config.js
@@ -2,8 +2,6 @@ module.exports = {
     client: {
       service: {
         name: 'my-app',
-        // URL to the GraphQL API
-        url: 'http://localhost:3000/graphql',
       },
       // Files processed by the extension
       includes: [
